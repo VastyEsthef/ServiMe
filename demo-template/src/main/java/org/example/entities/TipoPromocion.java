8ad0fbd5dd367a8623a27b9046749e644a08f6a7
Milagros1711 <milagrosmagallanes32@gmail.com>
2022-04-27 15:54:23 -0500

    Promocion y TipoPromocion

--- demo-template/src/main/java/org/example/entities/TipoPromocion.java
+++ demo-template/src/main/java/org/example/entities/TipoPromocion.java
@@ -1,5 +1,32 @@
 package org.example.entities;
 
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+@Entity
+@Table(name="tipoPromocion")
 public class TipoPromocion {
+	@Id
+	@GeneratedValue(strategy=GenerationType.IDENTITY)
+	private int id;
+	private String nombre;
+	
+	
+	public int getId() {
+		return id;
+	}
+	public void setId(int id) {
+		this.id = id;
+	}
+	public String getNombre() {
+		return nombre;
+	}
+	public void setNombre(String nombre) {
+		this.nombre = nombre;
+	}
+	
 
 }
